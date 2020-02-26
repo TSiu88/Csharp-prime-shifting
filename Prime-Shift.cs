fc56ed3efac354820388db0b16bb80aa3b6e7170
neha doodi and Tiffany Siu <tiffany@>
2020-02-26 13:44:21 -0800

    Add prime check with removing non-prime numbers, add comments

--- Prime-Shift.cs
+++ Prime-Shift.cs
@@ -7,6 +7,8 @@ namespace PrimeShifter{
     private int _endNumber;
     private int _endCheck;
     private List<int> _primes;
+
+    // Last number need to check is sqrt of number from user input
     public PrimeShift(int endNumber)
     {
       _endNumber = endNumber;
@@ -26,6 +28,8 @@ namespace PrimeShifter{
     public List<int> GetPrimes(){
       return _primes;
     }
+
+    //Create list with all numbers, not just prime numbers
     public void CreateAllNumbers()
     {
       for (int i = 2; i <= _endNumber; i++){
@@ -33,22 +37,21 @@ namespace PrimeShifter{
       }
     }
 
+    // Check for primes using prime^2 + prime(i) equation and removes from prime list if number is in list of all numbers
     public List<int> CheckForPrimes(){
       int checkIndex = 0;
       while(_primes[checkIndex] <= _endCheck){
         int primeToCheck = _primes[checkIndex];
         int primeSquared = Convert.ToInt32(Math.Pow(primeToCheck, 2));
+        // Finds for loop end value by reverse calculating the prime equation
         int endCheckValue = (int)Math.Ceiling((double)(_endNumber - primeSquared)/primeToCheck);
-        Console.WriteLine("end check Value" + endCheckValue);
 
         for(int i=0; i<=endCheckValue; i++){
           int numToRemove = primeSquared + (primeToCheck * i);
           if (numToRemove <= _primes[_primes.Count-1]){
-            Console.WriteLine("remove " + numToRemove);
+            _primes.Remove(numToRemove);
           }
-          
         }
-
         checkIndex++;
       }
       return _primes;
--- Program.cs
+++ Program.cs
@@ -5,17 +5,24 @@ using PrimeShifter;
 public class Program {
   static void Main()
   {
+    // Get input from user and quit if none entered
     Console.WriteLine("Find the primes up to what number?");
     string endString = Console.ReadLine();
+    if(endString == ""){
+      Environment.Exit(0);
+    }
     int endNumber = int.Parse(endString);
     PrimeShift primeShift = new PrimeShift(endNumber);
 
-
     Console.WriteLine("Find the primes to: " + primeShift.GetEndNumber() + " and end checks at " + primeShift.GetEndCheck());
-    for (int i=0; i< endNumber-1; i++){
-      Console.WriteLine(primeShift.GetPrimes()[i]);
-    }
     
+    // Check for primes and print output
     List<int> primeList = primeShift.CheckForPrimes();
+    Console.Write("[ ");
+    for (int j=0; j < primeList.Count; j++){
+      Console.Write(primeList[j] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
   }
 }
